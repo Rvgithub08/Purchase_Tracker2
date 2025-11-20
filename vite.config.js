8859ee5960617e0de5db93a54e2c09d87200dfe7
Rvgithub08 <rvravigodara@gmail.com>
2025-11-20 11:16:02 +0530

    Added deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Purchase_Tracker2/", // IMPORTANT!
   plugins: [react()],
 });
